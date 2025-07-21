deecab82a4b9db0889a9125707d0765cb4f9065f
Simon Harvey <simon.harvey.86@gmail.com>
2025-07-21 15:00:58 -0400

    feat: init push

--- infra/modules/azure_container_app/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "resource_group_name" {}
-variable "location" {}
-variable "container_image" {}
-variable "container_app_name" {}
--- infra/modules/cloudflare_dns/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "cloudflare_zone" {}
-variable "custom_domain" {}
-variable "cloudflare_api_token" {}
-variable "azure_fqdn" {}
